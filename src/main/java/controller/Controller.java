ec4f1141315d930cb3b2c965abbd16a88f55ed24
Guilherme Augusto <guilheaugusto1@gmail.com>
2021-12-19 11:40:53 -0300

    Controller - invocar o método inserirContato passando o objeto contato / redirecionar para o documento agenda.jsp. DAO - abrir a conexão / preparar a query para execução no banco de dados / substituir os parâmetros (?) pelo conteúdo das variáveis JavaBeans / executar a query / encerrar a conexão com o banco. (CREATE)

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -45,6 +45,10 @@ public class Controller extends HttpServlet {
 		contato.setNome(request.getParameter("nome"));
 		contato.setFone(request.getParameter("fone"));
 		contato.setEmail(request.getParameter("email"));
+		// invocar o método inserirContato passando o objeto contato
+		dao.inserirContato(contato);
+		// redirecionar para o documento agenda.jsp
+		response.sendRedirect("main");
 	}
 
 }

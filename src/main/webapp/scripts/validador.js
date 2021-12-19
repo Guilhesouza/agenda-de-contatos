35720d64218312e0e323ea0c6f54e04a23dc6d74
Guilherme Augusto <guilheaugusto1@gmail.com>
2021-12-19 10:11:26 -0300

    Incluindo a verificação no validador.js, caso não seja preenchido os campos obrigatórios, irá emitir um alerta, caso contrário, os dados serão enviador para o frmContato

--- src/main/webapp/scripts/validador.js
+++ src/main/webapp/scripts/validador.js
@@ -6,4 +6,15 @@
 function validar() {
 	let nome = frmContato.nome.value
 	let fone = frmContato.fone.value
+	if (nome === "") {
+		alerta('Preencha o campo Nome')
+		frmContato.nome.focus()
+		return false
+	} else if (fone === "") {
+		alerta('Preencha o campo Fone')
+		frmContato.fone.focus()
+		return false
+	} else {
+		document.forms["frmContato"].submit()
+	}
 }
